179bffd14a82e097fc64211359a29462dc9d2874
Carlos Pérez <carlosperez@Carloss-MacBook-Pro.local>
2020-11-05 14:26:21 -0600

    Initial Project

---
+++ ios/Pods/Target Support Files/Pods-ReactNativeSchoolCourse-ReactNativeSchoolCourseTests/Pods-ReactNativeSchoolCourse-ReactNativeSchoolCourseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeSchoolCourse_ReactNativeSchoolCourseTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeSchoolCourse_ReactNativeSchoolCourseTests
+@end
